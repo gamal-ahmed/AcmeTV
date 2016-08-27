53eedd21b23a53488bf44d16c30a6df7687cc29a
gamal-ahmed <agamal.halem@gmail.com>
2016-08-27 18:01:08 +0300

    Add DB Scripts

--- SQL/Script.sql
+++ SQL/Script.sql
@@ -1,6 +1,6 @@
-create database lulu;
+create database invoices_system;
 
-CREATE TABLE lulu.`invoice` (
+CREATE TABLE invoices_system.`invoice` (
   `invoiceId` int(11) NOT NULL AUTO_INCREMENT,
   `customerId` int(11) DEFAULT NULL,
   `addressId` int(11) DEFAULT NULL,
@@ -19,11 +19,11 @@ CREATE TABLE lulu.`invoice` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `invoice`(`customerId`,`addressId`,`invoiceType`,`invoiceTypeLocalized`,`invoiceDate`,`paymentDueDate`,`invoiceNumber`,`startDate`,`endDate`,`periodDescription`,`amount`,`vatAmount`,`totalAmount`) VALUES (2,8212154,'AdvancePayment','Voorschot','2016-02-02 00:00:00','2016-03-01 00:00:00','1','2016-08-01 01:00:00','2016-08-01 00:00:00','lulua','32','432','423423');
+INSERT INTO `invoice`(`customerId`,`addressId`,`invoiceType`,`invoiceTypeLocalized`,`invoiceDate`,`paymentDueDate`,`invoiceNumber`,`startDate`,`endDate`,`periodDescription`,`amount`,`vatAmount`,`totalAmount`) VALUES (2,8212154,'AdvancePayment','Voorschot','2016-02-02 00:00:00','2016-03-01 00:00:00','1','2016-08-01 01:00:00','2016-08-01 00:00:00','invoices_systema','32','432','423423');
 INSERT INTO `invoice`(`customerId`,`addressId`,`invoiceType`,`invoiceTypeLocalized`,`invoiceDate`,`paymentDueDate`,`invoiceNumber`,`startDate`,`endDate`,`periodDescription`,`amount`,`vatAmount`,`totalAmount`) VALUES (1,8212155,'ShopPurchase','q','2016-02-02 00:00:00','2016-03-01 00:00:00','2','2016-08-01 01:00:00','2016-08-01 00:00:00','w','2','3','4');
 
 
-CREATE TABLE lulu.`customer` (
+CREATE TABLE invoices_system.`customer` (
   `customerId` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
